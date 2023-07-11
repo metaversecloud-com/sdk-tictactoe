b2db7e6057e2fe39f5734ec6d456cd44607bd578
Saqib Khan <saqibmk@users.noreply.github.com>
2023-07-11 08:32:38 -0500

    Improvements and updates

--- server/index.ts
+++ server/index.ts
@@ -13,8 +13,6 @@ function checkEnvVariables() {
 
   if (missingVariables.length > 0) {
     throw new Error(`Missing required environment variables in the .env file: ${missingVariables.join(", ")}`);
-  } else {
-    console.log("All required environment variables provided.");
   }
 }
 checkEnvVariables();
@@ -25,6 +23,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/backend", router);
+
 if (process.env.NODE_ENV === "development") {
   const corsOptions = {
     origin: "http://localhost:3000",
@@ -34,20 +34,20 @@ if (process.env.NODE_ENV === "development") {
   app.use(cors(corsOptions));
 } else {
   // Node serves the files for the React app
-  const __filename = fileURLToPath(import.meta.url);
-  console.log("🚀 ~ file: index.js:44 ~ __filename:", __filename);
-  const __dirname = path.dirname(__filename);
-  console.log("🚀 ~ file: index.js:46 ~ __dirname:", __dirname);
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
-  console.log("🚀 ~ file: index.js:48 ~ path.resolve:", path.resolve(__dirname, "../client/build"));
+  const __filename = fileURLToPath(import.meta.url)
+  console.log("🚀 ~ file: index.js:44 ~ __filename:", __filename)
+  const __dirname = path.dirname(__filename)
+  console.log("🚀 ~ file: index.js:46 ~ __dirname:", __dirname)
+  app.use(express.static(path.resolve(__dirname, "../../client/dist")))
+  console.log("🚀 ~ file: index.js:48 ~ path.resolve:", path.resolve(__dirname, "../../client/dist"))
 
   // All other GET requests not handled before will return our React app
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
+      res.sendFile(path.resolve(__dirname, "../../client/dist", "index.html"));
+  })
 }
 
-app.use("/backend", router);
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
