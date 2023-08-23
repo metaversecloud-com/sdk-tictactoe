29c5ab8e16d18f0fdf9bb804ebbdae4d34c3d503
Alok Shukla <alok@clay.fish>
2023-08-24 01:39:33 +0530

    Updated common.ts and package-lock.json

--- server/utils/common.ts
+++ server/utils/common.ts
@@ -1,8 +1,7 @@
 // common functions
 
-import { DroppedAssetFactory, VisitorFactory, WorldFactory } from "@rtsdk/topia";
 import { Credentials } from "./types";
-import myTopiaInstance from "./topiaInstance";
+import { initDroppedAsset, initVisitor, initWorld } from "../topia/topia.factories";
 
 // returns the credentials from the query
 export const credentialsFromQuery = (req: any): Credentials => {
@@ -22,35 +21,9 @@ export const credentialsFromQuery = (req: any): Credentials => {
   };
 };
 
-
-
-export const extractImageURL = (url: string) => {
-  return `https://${url.split("https%3A//")[1].split("?")[0]}`
-}
-
-// GET DATA OBJECT FROM DROPPEDASSETID
-export const getDataObjectFromDroppedAsset = async (droppedAssetId: string, credentials: Credentials, key?: string) => {
-  try {
-    const asset = await new DroppedAssetFactory(myTopiaInstance).get(droppedAssetId, credentials.urlSlug, {
-      credentials,
-    });
-
-    const { dataObject } = asset;
-
-    if (!key) {
-      return dataObject;
-    }
-
-    return dataObject[key];
-  } catch (error) {
-    console.error("getDataObjectFromDroppedAsset: error", error);
-    return {};
-  }
-};
-
 export const getProfile = async (credentials: Credentials) => {
   try {
-    const visitor = await new VisitorFactory(myTopiaInstance).get(
+    const visitor = await initVisitor().get(
       parseInt(credentials.visitorId),
       credentials.urlSlug,
       {
@@ -68,7 +41,8 @@ export const getProfile = async (credentials: Credentials) => {
 };
 
 // GET PROFILE INFOMRATION FROM VISITORID
-export const getProfileInformationFromVisitorId = (visitorId: string) => {};
+export const getProfileInformationFromVisitorId = (visitorId: string) => {
+};
 
 // WRITE DATA OBJECT TO DROPPEDASSETID
 export const writeDataObjectToDroppedAssetId = async (credentials: Credentials, droppedAssetId: string, dataObject: any = {}) => {
@@ -77,14 +51,7 @@ export const writeDataObjectToDroppedAssetId = async (credentials: Credentials, 
   // or we need to update a key
   // or we need to delete a key
 
-  const writeObject = await new DroppedAssetFactory(myTopiaInstance).create(
-    credentials.assetId,
-    credentials.urlSlug,
-    {
-      credentials,
-    },
-  );
-
+  const writeObject = initDroppedAsset().create(credentials.assetId, credentials.urlSlug, { credentials });
   await writeObject.updateDataObject({ dataObject });
 
 };
@@ -94,7 +61,7 @@ export const writeDataObjectToDroppedAssetId = async (credentials: Credentials, 
 // returns an array where the key is the dropped assets or asset
 export const getDroppedAssetByNameFromSceneDropId = async (name: string[], credentials: Credentials) => {
   try {
-    const world = new WorldFactory(myTopiaInstance).create(credentials.urlSlug, { credentials });
+    const world = initWorld().create(credentials.urlSlug, { credentials });
     const { sceneDropIds } = (await world.fetchSceneDropIds()) as any;
     // only works for one scene drop id for now
     const assetsList = (await world.fetchDroppedAssetsBySceneDropId({ sceneDropId: sceneDropIds[0] })) as any;
