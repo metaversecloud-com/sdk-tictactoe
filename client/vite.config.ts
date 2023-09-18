7371cbdac24b2f37eda0b4f0ded48a64def9b5f5
Alok Shukla <alok@clay.fish>
2023-09-19 00:40:37 +0530

    Allowed all origins through cors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,7 @@ export default defineConfig({
     host: true,
     port: 3001,
     strictPort: true,
+    cors: true,
     proxy: {
       '/backend': {
         target: 'http://localhost:3000',
