bc1ce4864cf5f4f28dc4f44857fe7068cd333d57
Lina <linabell@mail.com>
2024-01-31 10:36:26 -0800

    remove console.log

--- src/utils/getVersion.ts
+++ src/utils/getVersion.ts
@@ -5,8 +5,6 @@ export const getVersion = () => {
     const packageJsonContent = fs.readFileSync("./package.json", "utf8");
     const packageJson = JSON.parse(packageJsonContent);
     const version = packageJson.version;
-
-    console.log("Version:", version);
     return version;
   } catch (error) {
     console.error("Error reading or parsing package.json:", error);
