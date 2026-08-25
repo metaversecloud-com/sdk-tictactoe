83c1a8af995b2189aa022c1763eac9671e3ecc5a
Lina <linabell@mail.com>
2026-08-25 11:26:06 -0700

    update info page

--- client/src/pages/InfoPage.tsx
+++ client/src/pages/InfoPage.tsx
@@ -1,19 +1,13 @@
-// components
-import { PageContainer } from "@/components";
-
 /**
  * Full-page How To Play. Entered from the canvas Info asset (link `/info`).
  */
 export const InfoPage = () => {
   return (
-    <PageContainer isLoading={false} headerText="How to Play">
-      <ol className="p2" style={{ paddingLeft: "1.25rem", listStyle: "decimal" }}>
-        <li>Two players click the pink X or blue O sprite on the board to claim a symbol.</li>
-        <li>Once both symbols are claimed, whoever is picked first moves first.</li>
-        <li>Click cells on the board to place your mark. Get three in a row to win.</li>
-        <li>Click the Reset asset when you want to start a new round.</li>
-      </ol>
-    </PageContainer>
+    <img
+      alt="How To Play"
+      className="w-full h-full object-cover"
+      src="https://sdk-tictactoe.s3.us-east-1.amazonaws.com/instructions.png"
+    />
   );
 };
 
